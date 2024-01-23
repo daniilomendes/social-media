881c511f2d16dab625dec2f992f797f79f148d6d
Danilo Mendes <danilo.mirandha@gmail.com>
2024-01-23 19:01:21 -0300

    chore: setup inicial do projeto

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import {Routes, Route} from 'react-router-dom'
+import "./globals.css"
+
+const App = () => {
+  return (
+    <main className='flex h-screen'>
+        <Routes>
+            {/* public routes */}
+            
+            {/* private routes */}
+        </Routes>
+    </main>
+  )
+}
+
+export default App
