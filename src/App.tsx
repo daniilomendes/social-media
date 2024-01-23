851d6143acc7ffd95a3757bce5191b5dd4845326
Danilo Mendes <danilo.mirandha@gmail.com>
2024-01-23 19:17:46 -0300

    chore: garantindo que as rotas publicas e privadas estão recebendo seus layouts

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,28 @@
-import {Routes, Route} from 'react-router-dom'
-import "./globals.css"
+import { Routes, Route } from "react-router-dom";
+import SigninForm from "./_auth/forms/SigninForm";
+import SignupForm from "./_auth/forms/SignupForm";
+import AuthLayout from "./_auth/AuthLayout";
+import RootLayout from "./_root/RootLayout";
+import { Home } from "./_root/pages";
+import "./globals.css";
 
 const App = () => {
   return (
-    <main className='flex h-screen'>
-        <Routes>
-            {/* public routes */}
-            
-            {/* private routes */}
-        </Routes>
-    </main>
-  )
-}
+    <main className="flex h-screen">
+      <Routes>
+        {/* public routes */}
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<SigninForm />} />
+          <Route path="/sign-up" element={<SignupForm />} />
+        </Route>
 
-export default App
+        {/* private routes */}
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </main>
+  );
+};
+
+export default App;
