f3e81e5f0e2ac27c2fee5dd6820d1faa202f2d44
Danilo Mendes <danilo.mirandha@gmail.com>
2024-01-23 20:19:28 -0300

    chore: adicionando setup para utilizar e validar o formulário do shadcn

---
+++ src/lib/validation/index.ts
@@ -0,0 +1,8 @@
+import * as z from "zod";
+
+export const SignupValidation = z.object({
+  name: z.string().min(2, { message: "Muito curto" }),
+  username: z.string().min(2, { message: "Muito curto" }),
+  email: z.string().email(),
+  password: z.string().min(8, { message: "No mínimo 8 caracteres!" }),
+});
