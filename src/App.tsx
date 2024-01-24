97b95a4b9e697d6669401e2e7fc0cf8da1cab002
Danilo Mendes <danilo.mirandha@gmail.com>
2024-01-24 18:39:09 -0300

    feat: setup para utilizar o toast component do shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import AuthLayout from "./_auth/AuthLayout";
 import RootLayout from "./_root/RootLayout";
 import { Home } from "./_root/pages";
 import "./globals.css";
+import { Toaster } from "@/components/ui/toaster"
 
 const App = () => {
   return (
@@ -21,6 +22,8 @@ const App = () => {
           <Route index element={<Home />} />
         </Route>
       </Routes>
+
+      <Toaster />
     </main>
   );
 };
--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -1,5 +1,6 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
+import { useToast } from "@/components/ui/use-toast";
 import {
   Form,
   FormControl,
@@ -17,6 +18,7 @@ import { Link } from "react-router-dom";
 import { createUserAccount } from "@/lib/appwrite/api";
 
 const SignupForm = () => {
+  const { toast } = useToast();
   const isLoading = false;
 
   const form = useForm<z.infer<typeof SignupValidation>>({
@@ -30,9 +32,13 @@ const SignupForm = () => {
   });
 
   async function onSubmit(values: z.infer<typeof SignupValidation>) {
-    const newUser = await createUserAccount(values)
+    const newUser = await createUserAccount(values);
 
-    console.log(newUser)
+    if (!newUser) {
+      return toast({ title: 'Cadastro não finalizado. Tente novamente.'});
+    }
+
+    // const session = await signInAccount()
   }
 
   return (
