3b37741df04d907a6fdb8ab8321e8c694eeb2dc2
Danilo Mendes <danilo.mirandha@gmail.com>
2024-01-26 20:20:16 -0300

    feat: adicionando página de detalhes do post

--- src/components/shared/PostCard.tsx
+++ src/components/shared/PostCard.tsx
@@ -1,7 +1,8 @@
-import { useUserContext } from "@/context/AuthContext";
-import { multiFormatDateString } from "@/lib/utils";
 import { Models } from "appwrite";
 import { Link } from "react-router-dom";
+
+import { multiFormatDateString } from "@/lib/utils";
+import { useUserContext } from "@/context/AuthContext";
 import PostStats from "./PostStats";
 
 type PostCardProps = {
@@ -20,22 +21,23 @@ const PostCard = ({ post }: PostCardProps) => {
           <Link to={`/profile/${post.creator.$id}`}>
             <img
               src={
-                post?.creator?.imageUrl ||
-                "assets/icons/profile-placeholder.svg"
+                post.creator?.imageUrl ||
+                "/assets/icons/profile-placeholder.svg"
               }
               alt="criador"
-              className="rounded-full w-12 lg:h12"
+              className="w-12 lg:h-12 rounded-full"
             />
           </Link>
+
           <div className="flex flex-col">
             <p className="base-medium lg:body-bold text-light-1">
               {post.creator.name}
             </p>
             <div className="flex-center gap-2 text-light-3">
-              <p className="subtle-semibold lg:small-regular">
+              <p className="subtle-semibold lg:small-regular ">
                 {multiFormatDateString(post.$createdAt)}
               </p>
-              -
+              •
               <p className="subtle-semibold lg:small-regular">
                 {post.location}
               </p>
@@ -47,7 +49,12 @@ const PostCard = ({ post }: PostCardProps) => {
           to={`/update-post/${post.$id}`}
           className={`${user.id !== post.creator.$id && "hidden"}`}
         >
-          <img src="/assets/icons/edit.svg" alt="edit" width={20} height={20} />
+          <img
+            src={"/assets/icons/edit.svg"}
+            alt="editar"
+            width={20}
+            height={20}
+          />
         </Link>
       </div>
 
@@ -55,8 +62,8 @@ const PostCard = ({ post }: PostCardProps) => {
         <div className="small-medium lg:base-medium py-5">
           <p>{post.caption}</p>
           <ul className="flex gap-1 mt-2">
-            {post.tags.map((tag: string) => (
-              <li key={tag} className="text-light-3">
+            {post.tags.map((tag: string, index: string) => (
+              <li key={`${tag}${index}`} className="text-light-3 small-regular">
                 #{tag}
               </li>
             ))}
