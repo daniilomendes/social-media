47d330931fd787b6de6010e4d3dcf7b0eaf698cd
Danilo Mendes <danilo.mirandha@gmail.com>
2024-01-27 08:04:32 -0300

    fix: parametros que o tanquery necessitava para melhor funcionamento

--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -171,13 +171,15 @@ export const usedeletePost = () => {
 export const useGetPosts = () => {
   return useInfiniteQuery({
     queryKey: [QUERY_KEYS.GET_INFINITE_POSTS],
-    queryFn: getInfinitePosts,
-    getNextPageParam: (lastPage) => {
-      if (lastPage && lastPage.documents.length === 0) return null;
+    queryFn: getInfinitePosts as any,
+    initialPageParam: 0,
+    getNextPageParam: (lastPage: any) => {
+      if (lastPage && lastPage.documents.length === 0) {
+        return null;
+      }
 
-      const lasId = lastPage.documents[lastPage?.documents.length - 1].$id;
-
-      return lasId;
+      const lastId = lastPage.documents[lastPage.documents.length - 1].$id;
+      return lastId;
     },
   });
 };
