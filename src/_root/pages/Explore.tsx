ce74ab2ffaded871f48f1e9ecad33be2bf71ffb2
Danilo Mendes <danilo.mirandha@gmail.com>
2024-01-26 22:17:12 -0300

    feat: garantindo que a página de explorar e o sistema de busca está funcioando

--- src/_root/pages/Explore.tsx
+++ src/_root/pages/Explore.tsx
@@ -1,7 +1,97 @@
-const Explore = () => {
-  return (
-    <div>Explore</div>
-  )
-}
+import GridPostList from "@/components/shared/GridPostList";
+import Loader from "@/components/shared/Loader";
+import SearchResults from "@/components/shared/SearchResults";
+import { Input } from "@/components/ui/input";
+import useDebounce from "@/hooks/useDebounce";
+import {
+  useGetPosts,
+  useSearchPosts,
+} from "@/lib/react-query/queriesAndMutations";
+import { useEffect, useState } from "react";
+import { useInView } from "react-intersection-observer";
 
-export default Explore
+const Explore = () => {
+  const { ref, inView } = useInView({});
+  const { data: posts, fetchNextPage, hasNextPage } = useGetPosts();
+
+  const [searchValue, setSearchValue] = useState("");
+  const devounceValue = useDebounce(searchValue, 500);
+  const { data: searchedPosts, isFetching: isSearchFetching } =
+    useSearchPosts(devounceValue);
+
+  useEffect(() => {
+    if (inView && !searchValue) fetchNextPage();
+  }, [inView]);
+
+  if (!posts) {
+    return (
+      <div className="flex-center w-full h-full">
+        <Loader />
+      </div>
+    );
+  }
+
+  const shouldShowSearchResult = searchValue !== "";
+  const shouldShowPosts =
+    !shouldShowSearchResult &&
+    posts?.pages.every((item) => item.documents.length === 0);
+
+  return (
+    <div className="explore-container">
+      <div className="explore-inner_container">
+        <h2 className="h3-bold md:bold w-full">Buscar Posts</h2>
+        <div className="flex gap-1 px-4 w-full rounded-lg bg-dark-4">
+          <img
+            src="/assets/icons/search.svg"
+            alt="buscar"
+            width={24}
+            height={24}
+          />
+          <Input
+            type="text"
+            placeholder="Buscar"
+            className="explore-search"
+            value={searchValue}
+            onChange={(e) => setSearchValue(e.target.value)}
+          />
+        </div>
+      </div>
+      <div className="flex-between w-full max-w-5xl mt-16 mb-7">
+        <h2 className="body-bold md:h3-bold">Popular hoje</h2>
+        <div className="flex-center gap-3 bg-dark-3 rounded-xl px-4 py-2 cursor-pointer">
+          <p className="small-medium md:base-medium text-light-2">Todos</p>
+          <img
+            src="/assets/icons/filter.svg"
+            alt="filtro"
+            width={20}
+            height={20}
+          />
+        </div>
+      </div>
+      <div className="flex flex-wrap gap-9 w-full max-w-5xl">
+        {shouldShowSearchResult ? (
+          <SearchResults
+            isSearchFetching={isSearchFetching}
+            searchedPosts={searchedPosts}
+          />
+        ) : shouldShowPosts ? (
+          <p className="text-light-4 mt-10 text-center w-full">
+            Fim das postagens
+          </p>
+        ) : (
+          posts.pages.map((item, index) => (
+            <GridPostList key={`page-${index}`} posts={item.documents} />
+          ))
+        )}
+      </div>
+
+      {hasNextPage && !searchValue && (
+        <div ref={ref} className="mt-10">
+          <Loader />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Explore;
